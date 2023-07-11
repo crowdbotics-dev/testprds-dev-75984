5078525f59be18ec538919b5b3ef8cd19872a718
Crowdbotics <engineering@crowdbotics.com>
2023-07-11 11:06:27 +0000

    Update store.

--- store/testprdsdevAPI/api.js
+++ store/testprdsdevAPI/api.js
@@ -9,13 +9,13 @@ function api_docs_schema_retrieve(payload) {
   })
 }
 function api_v1_login_create(payload) {
-  return testprdsdevAPI.post(`/api/v1/login/`, payload.data)
+  return testprdsdevAPI.post(`/api/v1/login/`, payload)
 }
 function api_v1_signup_create(payload) {
-  return testprdsdevAPI.post(`/api/v1/signup/`, payload.data)
+  return testprdsdevAPI.post(`/api/v1/signup/`, payload)
 }
 function rest_auth_login_create(payload) {
-  return testprdsdevAPI.post(`/rest-auth/login/`, payload.data)
+  return testprdsdevAPI.post(`/rest-auth/login/`, payload)
 }
 function rest_auth_logout_retrieve(payload) {
   return testprdsdevAPI.get(`/rest-auth/logout/`)
@@ -24,31 +24,28 @@ function rest_auth_logout_create(payload) {
   return testprdsdevAPI.post(`/rest-auth/logout/`)
 }
 function rest_auth_password_change_create(payload) {
-  return testprdsdevAPI.post(`/rest-auth/password/change/`, payload.data)
+  return testprdsdevAPI.post(`/rest-auth/password/change/`, payload)
 }
 function rest_auth_password_reset_create(payload) {
-  return testprdsdevAPI.post(`/rest-auth/password/reset/`, payload.data)
+  return testprdsdevAPI.post(`/rest-auth/password/reset/`, payload)
 }
 function rest_auth_password_reset_confirm_create(payload) {
-  return testprdsdevAPI.post(`/rest-auth/password/reset/confirm/`, payload.data)
+  return testprdsdevAPI.post(`/rest-auth/password/reset/confirm/`, payload)
 }
 function rest_auth_registration_create(payload) {
-  return testprdsdevAPI.post(`/rest-auth/registration/`, payload.data)
+  return testprdsdevAPI.post(`/rest-auth/registration/`, payload)
 }
 function rest_auth_registration_verify_email_create(payload) {
-  return testprdsdevAPI.post(
-    `/rest-auth/registration/verify-email/`,
-    payload.data
-  )
+  return testprdsdevAPI.post(`/rest-auth/registration/verify-email/`, payload)
 }
 function rest_auth_user_retrieve(payload) {
   return testprdsdevAPI.get(`/rest-auth/user/`)
 }
 function rest_auth_user_update(payload) {
-  return testprdsdevAPI.put(`/rest-auth/user/`, payload.data)
+  return testprdsdevAPI.put(`/rest-auth/user/`, payload)
 }
 function rest_auth_user_partial_update(payload) {
-  return testprdsdevAPI.patch(`/rest-auth/user/`, payload.data)
+  return testprdsdevAPI.patch(`/rest-auth/user/`, payload)
 }
 export const apiService = {
   api_docs_schema_retrieve,
